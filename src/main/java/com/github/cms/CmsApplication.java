ae4140d575cf443ae9295ff38569ff7f28dc100a
zhangmingyang <zmy@dxct.org>
2020-01-13 18:51:22 +0800

    启动文件增加时区启动信息

--- src/main/java/com/github/cms/CmsApplication.java
+++ src/main/java/com/github/cms/CmsApplication.java
@@ -3,11 +3,19 @@ package com.github.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CmsApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(CmsApplication.class, args);
     }
+    @PostConstruct
 
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
 }
